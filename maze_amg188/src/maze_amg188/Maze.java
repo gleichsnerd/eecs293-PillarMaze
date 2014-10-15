cccc5f76bd1c0b656ffcf578d526fe199b8c80c0
Adam Gleichsner <amg188@case.edu>
2014-10-14 22:28:36 -0400

    Added maze generation, changed PillarNodes to be identified by an ID number instead of (x,y) position

--- maze_amg188/src/maze_amg188/Maze.java
+++ maze_amg188/src/maze_amg188/Maze.java
@@ -1,21 +1,28 @@
 package maze_amg188;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Maze {
 
-	Array<ArrayList<PillarNode>> pillars;
+	ArrayList<ArrayList<PillarNode>> pillars;
 	
-	public Maze(int width, int height, ArrayList<ArrayList<PillarNode>> planks) {
-		this.pillars = this.createMazePillars(width, height, planks);
+	public Maze(int width, int height, Map<Integer, ArrayList<Integer>> plankLayout) {
+		this.pillars = this.createMazePillars(width, height, plankLayout);
 	}
 	
-	private ArrayList<ArrayList<PillarNode>> createMazePillars(int width, int height, ArrayList<ArrayList<PillarNode>> planks) {
+	private ArrayList<ArrayList<PillarNode>> createMazePillars(int width, int height, Map<Integer, ArrayList<Integer>> plankLayout) {
 		ArrayList<ArrayList<PillarNode>> returnPillars = new ArrayList<ArrayList<PillarNode>>();
+		ArrayList<Integer> accessiblePillars;
+		PillarNode newPillar;
 		
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
-				if ()
+				newPillar = new PillarNode(y * height + x, -1, -1, -1);
+				accessiblePillars = getPillarsAccessibleByPlanks(x, y, width, height, plankLayout);
+				newPillar.addPlanks(accessiblePillars);
+				returnPillars.get(y).add(newPillar);
+				
 			}
 		}
 		
@@ -23,6 +30,41 @@ public class Maze {
 
 	}
 	
-	private ArrayList<>getPillarsAccessibleByPlanks
+//	private int calculateHValue()
+	
+	private ArrayList<Integer> getPillarsAccessibleByPlanks(int x, int y, int width, int height, Map<Integer, ArrayList<Integer>> plankLayout) {
+		ArrayList<Integer> returnPillars = new ArrayList<Integer>(); //List of all pillars accessible, identified by number
+		Integer currentPillarID = new Integer(y * height + x);
+		/* Note: Single number id is found by row * height + column, resulting in the following layout
+		 * row _0__1__2__3    <- column
+		 *   0| 0  1  2  3
+		 *   1| 4  5  6  7
+		 *   2| 8  9  10 11
+		 *   3| 12 13 14 15
+		 */
+		
+		if (plankLayout.size() > width * height) {
+			//If we have more pillars in the layout than possible, throw an exception
+			throw new IllegalArgumentException("plankLayout contains more pillars than the maze can hold");
+		} 
+		
+		if (returnPillars.contains(currentPillarID)) {
+			returnPillars = plankLayout.get(currentPillarID);
+			
+			for (Integer accessiblePillar: returnPillars) {
+				if (!pillarsAreAdjoining(currentPillarID, accessiblePillar, width, height)) {
+					throw new IllegalArgumentException("plankLayout contains an invalid configuration");
+				}
+			}
+		}
+		
+		return returnPillars;
+		
+	}
 
+	private boolean pillarsAreAdjoining(int pillar1ID, int pillar2ID, int width, int height) {
+		if (Math.abs(pillar1ID - pillar2ID) == height || (Math.abs(pillar1ID - pillar2ID) == 1 && Math.floor(pillar1ID/height) == Math.floor(pillar2ID/height)))
+				return true;
+		return false;
+	}
 }
--- maze_amg188/src/maze_amg188/PillarNode.java
+++ maze_amg188/src/maze_amg188/PillarNode.java
@@ -4,23 +4,22 @@ import java.util.ArrayList;
 
 public class PillarNode {
 	
-	int xPos;
-	int yPos;
+	int id; //
 	int hValue; //Heuristic value, steps away from the exit
 	int gValue; //Number of steps between start node and this node without using planks
     int pValue; //Number of steps between start node and this node with using a plank
     PillarNode gParent; //Parent node when plank is not used
     PillarNode pParent; //Parent node when plank is used
-    ArrayList<PillarNode> planks; //List of all immediately accessible nodes from the current node
+    ArrayList<Integer> planks; //List of all immediately accessible nodes from the current node
  
-	public PillarNode(int hValue, int gValue, int pValue) {
+	public PillarNode(int id, int hValue, int gValue, int pValue) {
 		this.hValue = hValue;
 		this.gValue = gValue;
 		this.pValue = pValue;
 		
 		this.gParent = null;
 		this.pParent = null;
-		this.planks = new ArrayList<PillarNode>();
+		this.planks = new ArrayList<Integer>();
 	}
 	
 	public int getHValue() {
@@ -63,7 +62,7 @@ public class PillarNode {
 		this.pParent = pParent;
 	}
 	
-	public ArrayList<PillarNode> getPlanks() {
+	public ArrayList<Integer> getPlanks() {
 		return this.planks;
 	}
 	
@@ -73,13 +72,23 @@ public class PillarNode {
 		return false;
 	}
 	
-	public boolean addPlank(PillarNode pillar) {
-		this.throwExceptionForNullInput(pillar);
-		if(this.planks.contains(pillar))
+	private boolean addPlank(int pillarID) {
+		this.throwExceptionForNullInput(pillarID);
+		if(this.planks.contains(pillarID))
 			return false;
 	
-		planks.add(pillar);
+		planks.add(pillarID);
 		return true;
+	}
+	
+	public boolean addPlanks(ArrayList<Integer> accessiblePillars) {
+		boolean returnBool = false;
+		
+		for (Integer pillarID: accessiblePillars) {
+			returnBool = this.addPlank(pillarID);
+		}
+		
+		return returnBool;
 	}
 	
 	public void throwExceptionForNullInput(Object...objects) throws NullPointerException {
